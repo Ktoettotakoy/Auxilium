a5b3695ae90ed3bc31a3143776164c20ede48f4e
Ktoettotakoy <98839231+Ktoettotakoy@users.noreply.github.com>
2024-10-12 10:28:39 +0100

    added front/back separation and npm commands to run both

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./client/index.css";
-import App from "./client/App";
-import reportWebVitals from "./client/reportWebVitals";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
